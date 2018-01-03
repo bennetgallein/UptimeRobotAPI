385a4b5b9f4afa7b5719126a1086fb9422dc604a
Bennet Gallein <bennet@intranetproject.net>
2018-01-03 03:20:52 +0100

    added monitor object

--- src/Monitor.php
+++ src/Monitor.php
@@ -10,45 +10,172 @@ namespace UptimeRobot;
 
 class Monitor {
 
-    private $apik;
-    private $format;
+    private $id;
 
-    const JSON = 'json';
-    const XML = 'xml';
 
-    public function __construct($apikey) {
-        $this->apik = $apikey;
+    private $friendly_name;
+    private $url;
+    private $type;
+    private $sub_type;
+    private $keyword_type;
+    private $keyword_value;
+    private $http_username;
+    private $http_password;
+    private $port;
+    private $interval;
+    private $status;
+    private $all_time_uptime_ratio;
+    private $custom_uptime_ratios;
+    private $custom_uptime_ranges;
+    private $average_respone_time;
+
+    const PAUSED = 2;
+    const NOT_CHECKED_YET = 1;
+    const UP = 2;
+    const SEEMS_DOWN = 8;
+    const DOWN = 9;
+
+    public function __construct($data) {
+        $this->id = $data->id;
+        $this->friendly_name = $data->friendly_name;
+        $this->url = $data->url;
+        $this->type = $data->type;
+        $this->sub_type = $data->sub_type;
+        $this->keyword_type = $data->keyword_type;
+        $this->keyword_value = $data->keyword_value;
+        $this->http_username = $data->http_username;
+        $this->http_password = $data->http_password;
+        $this->port = $data->port;
+        $this->interval = $data->interval;
+        $this->status = $data->status;
+        $this->all_time_uptime_ratio = isset($data->all_time_uptime_ration) ? $data->all_time_uptime_ration : null;
+        $this->custom_uptime_ratios = isset($data->custom_uptime_ratios) ? $data->custom_uptime_ratios : null;
+        $this->custom_uptime_ranges = isset($data->custom_uptime_ranges) ? $data->custom_uptime_ranges : null;
+        $this->average_respone_time = isset($data->average_respone_time) ? $data->average_respone_time : null;
     }
 
-    public function request($query) {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.uptimerobot.com/v2/$query",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "api_key=$this->apik&format=$this->format",
-            CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "content-type: application/x-www-form-urlencoded"
-            ),
-        ));
+    /**
+     * @return mixed
+     */
+    public function getID() {
+        return $this->id;
+    }
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+    /**
+     * @return mixed
+     */
+    public function getFriendlyName() {
+        return $this->friendly_name;
+    }
 
-        curl_close($curl);
+    /**
+     * @return mixed
+     */
+    public function getURL() {
+        return $this->url;
+    }
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
+    /**
+     * @return mixed
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubType() {
+        return $this->sub_type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeywordType() {
+        return $this->keyword_type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeywordValue() {
+        return $this->keyword_value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHttpUsername() {
+        return $this->http_username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHttpPassword() {
+        return $this->http_password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPort() {
+        return $this->port;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInterval() {
+        return $this->interval;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus() {
+        $reflect = new \ReflectionClass('\UptimeRobot\Monitor');
+        $constants = $reflect->getConstants();
+
+        $constName = null;
+        foreach ( $constants as $name => $value )
+        {
+            if ( $value == $this->status )
+            {
+                $constName = $name;
+                break;
+            }
         }
+
+        return $constName;
     }
-    public function setResponseFormat($newFormat) {
-        $this->format = $newFormat;
+
+    /**
+     * @return mixed
+     */
+    public function getAllTimeUptimeRatio() {
+        return $this->all_time_uptime_ratio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomUptimeRatios() {
+        return $this->custom_uptime_ratios;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomUptimeRanges() {
+        return $this->custom_uptime_ranges;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAverageResponeTime() {
+        return $this->average_respone_time;
     }
 }
--- src/UptimeRobot.php
+++ src/UptimeRobot.php
@@ -20,8 +20,18 @@ class UptimeRobot {
         $this->apik = $apikey;
     }
 
-    private function request($query) {
+    private function request($query, $parameter) {
+        if (!(isset($parameter))) {
+            $parameter = array("api_key" => $this->apik, "format" => $this->format);
+        }
         $curl = curl_init();
+
+        $postbody = "";
+
+        foreach ($parameter as $key => $item) {
+            $postbody .= $key . "=" . $item . "&";
+        }
+
         curl_setopt_array($curl, array(
             CURLOPT_URL => "https://api.uptimerobot.com/v2/$query",
             CURLOPT_RETURNTRANSFER => true,
@@ -30,7 +40,7 @@ class UptimeRobot {
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "api_key=$this->apik&format=$this->format",
+            CURLOPT_POSTFIELDS => $postbody,
             CURLOPT_HTTPHEADER => array(
                 "cache-control: no-cache",
                 "content-type: application/x-www-form-urlencoded"
@@ -48,10 +58,53 @@ class UptimeRobot {
             return $response;
         }
     }
+
     public function setResponseFormat($newFormat) {
         $this->format = $newFormat;
     }
+
     public function getAccountDetails() {
         return $this->request("getAccountDetails");
+    }
+
+    /**
+     * @param $parameter
+     * @return mixed
+     *
+     * api_key - required
+     * monitors - optional (if not used, will return all monitors in an account. Else, it is possible to define any number of monitors with their IDs like: monitors=15830-32696-83920)
+     * types - optional (if not used, will return all monitors types (HTTP, keyword, ping..) in an account. Else, it is possible to define any number of monitor types like: types=1-3-4)
+     * statuses - optional (if not used, will return all monitors statuses (up, down, paused) in an account. Else, it is possible to define any number of monitor statuses like: statuses=2-9)
+     * custom_uptime_ratios - optional (defines the number of days to calculate the uptime ratio(s) for. Ex: custom_uptime_ratios=7-30-45 to get the uptime ratios for those periods)
+     * custom_uptime_ranges - optional (defines the ranges to calculate the uptime ratio(s) for. Ex: custom_uptime_ranges=1465440758_1466304758 to get the uptime ratios for those periods. It is possible to send multiple ranges like 1465440758_1466304758-1434682358_1434855158)
+     * all_time_uptime_ratio - optional (returns the "all time uptime ratio". It will slow down the response a bit and, if not really necessary, suggest not using it. Default is 0)
+     * all_time_uptime_durations - optional (returns the "all time durations of up-down-paused events". It will slow down the response a bit and, if not really necessary, suggest not using it. Default is 0)
+     * logs - optional (defines if the logs of each monitor will be returned. Should be set to 1 for getting the logs. Default is 0)
+     * logs_start_date - optional and works only for the Pro Plan as 24 hour+ logs are kept only in the Pro Plan (starting date of the response times, formatted as Unix time and must be used with response_times_end_date) (can only be used if monitors parameter is used with a single id and response_times_end_date - response_times_start_date can't be more than 7 days)
+     * logs_end_date - optional and works only for the Pro Plan as 24 hour+ logs are kept only in the Pro Plan (ending date of the response times, formatted as Unix time and must be used with response_times_start_date) (can only be used if monitors parameter is used with a single id and response_times_end_date - response_times_start_date can't be more than 7 days)
+     * logs_limit - optional (the number of logs to be returned (descending order). If empty, all logs are returned.
+     * response_times - optional (defines if the response time data of each monitor will be returned. Should be set to 1 for getting them. Default is 0)
+     * response_times_limit - optional (the number of response time logs to be returned (descending order). If empty, last 24 hours of logs are returned (if response_times_start_date and response_times_end_date are not used).
+     * response_times_average - optional (by default, response time value of each check is returned. The API can return average values in given minutes. Default is 0. For ex: the Uptime Robot dashboard displays the data averaged/grouped in 30 minutes)
+     * response_times_start_date - optional (starting date of the logs, formatted as Unix time and must be used with logs_end_date) (can only be used if monitors parameter is used with a single id)
+     * response_times_end_date - optional (ending date of the logs, formatted as Unix time and must be used with logs_start_date) (can only be used if monitors parameter is used with a single id)
+     * alert_contacts - optional (defines if the alert contacts set for the monitor to be returned. Default is 0)
+     * mwindows - optional (the maintenance windows for the monitor which can be mentioned with their IDs like 345-2986-71)
+     * timezone - optional (defines if the user's timezone should be returned. Should be set to 1 for getting it. Default is 0)
+     * offset - optional (used for pagination. Defines the record to start paginating. Default is 0)
+     * limit - optional (used for pagination. Defines the max number of records to return for the response. Default and max. is 50)
+     * search - optional (a keyword of your choice to search within url and friendly_name and get filtered results)
+     *
+     * overgive params in an array like:
+     *
+     * getMonitors(["api_key" => "asd"]);
+     */
+    public function getMonitors($parameter) {
+        $monitorsarray =  json_decode($this->request("getMonitors", $parameter));
+        $monitors = [];
+        foreach ($monitorsarray->monitors as $monitor) {
+            $monitors[] = new Monitor($monitor);
+        }
+        return $monitors;
     }
 }
