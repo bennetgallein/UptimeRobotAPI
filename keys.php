f086799aec200005aebd71650e9ce8d7dcb63264
Bennet Gallein <bennet@intranetproject.net>
2018-02-04 15:19:08 +0100

    Composer inti

--- keys.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Bennet
- * Date: 1/2/2018
- * Time: 12:04 AM
- */
-$__account_key = "u531359-d53d1027d75f620c375f7298";
-$__monitor_key = "m779812078-83951b9684ddd9729899c6d6";
